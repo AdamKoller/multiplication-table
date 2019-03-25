3615b03bfefb913d6fe8513493977e17d8994243
Adam Koller <41529662+AdamKoller@users.noreply.github.com>
2019-03-24 22:29:57 -0500

    finished js

--- math.js
+++ math.js
@@ -1,10 +1,3 @@
-// start with two variables:
-//  1) number holds the number that the clculations will be performed with (in this case the number 15)
-//  2) operator indicates whether it should be addition or mulipplication (in this case it is performing multiplication)
-
-// an IF statement is used to decide to perform add. or mult. with the number
-// inside the conditional statement, a while loop is used to calculate the results
-
 var table = 15;
 var operator = 'multiplication';
 var i = 2;
@@ -12,9 +5,14 @@ var msg = '';
 
 if (operator === 'multiplication') {
   while (i < 11) {
-    msg += i + ' * ' + table + ' = ' + (i * table) + '<br />';
+    msg += i + ' x ' + table + ' = ' + (i * table) + '<br/>';
     i++;
   }
 } else {
-  while ()
+  while (i < 11) {
+    msg += i + ' + ' + table + ' = ' + (i + table) + '<br/>';
+    i++;
+  }
 }
+ var el = document.getElementById('chalkboard');
+ el.innerHTML = msg;
